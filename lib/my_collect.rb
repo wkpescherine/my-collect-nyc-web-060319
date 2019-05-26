5e66b6abd8cd702a7a7ddf73e3ff4b048f5166fe
William Pescherine <wkpescherine@gmail.com>
2019-05-26 05:07:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
 
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
 
